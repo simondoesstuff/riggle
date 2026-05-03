7412ace9f4de6312a45882816303e5191f59a69d
Simon Walker <simon@simonwalker.tech>
2026-05-03 10:10:49 -0600

    chore: stress test

--- scripts/at_scale_many.sh
+++ scripts/at_scale_many.sh
@@ -1,10 +1,11 @@
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 2 -n 5000 -m 1000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 2 -n 50000 -m 10000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 5 -n 200000 -m 100000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 10 -n 200000 -m 200000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 20 -n 200000 -m 400000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 50 -n 200000 -m 1000000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 100 -n 200000 -m 2000000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 200 -n 200000 -m 4000000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 500 -n 200000 -m 10000000 -t 600 -b 400
-rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 1000 -n 1000000 -m 100000000 -t 1200 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 2 -n 5000 -m 1000 -t 600 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 2 -n 50000 -m 10000 -t 600 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 5 -n 200000 -m 100000 -t 600 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 10 -n 200000 -m 200000 -t 600 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 20 -n 200000 -m 400000 -t 600 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 50 -n 200000 -m 1000000 -t 1200 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 100 -n 200000 -m 2000000 -t 1200 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 200 -n 200000 -m 4000000 -t 2400 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 500 -n 200000 -m 10000000 -t 3600 -b 400
+# rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 1000 -n 1000000 -m 100000000 -t 3600 -b 400
+rm -rf data/at_scale_bench_* && ./scripts/at_scale_bench.sh -f 2000 -n 1000000 -m 200000000 -t 3600 -b 400
