6da864dca47f39cf2f360369cba7bdb36c012946
Simon Walker <simon@simonwalker.tech>
2026-05-16 19:21:28 -0600

    feat: fourier-based pvals integration

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod bench;
 pub mod core;
+pub mod fourier;
 pub mod io;
 pub mod matrix;
 pub mod stats;
--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -2,4 +2,4 @@ pub mod build;
 pub mod query;
 
 pub use build::{AddConfig, AddError, add_to_database};
-pub use query::{QueryConfig, QueryError, QueryResult, QuerySource, query_database};
+pub use query::{PValueResult, QueryConfig, QueryError, QueryResult, QuerySource, query_database};
